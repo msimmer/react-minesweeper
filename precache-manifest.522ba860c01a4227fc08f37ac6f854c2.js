ddd38102f359349ccb9ae96e1f3fb9dd41cf18aa
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-04-16 16:47:09 +0200

    Updates

---
+++ precache-manifest.522ba860c01a4227fc08f37ac6f854c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "cc7afc37f6738fe44f7c",
+    "url": "/static/js/main.9d87c0e3.chunk.js"
+  },
+  {
+    "revision": "e41f67eda0c12477215f",
+    "url": "/static/js/2.ed46d7f6.chunk.js"
+  },
+  {
+    "revision": "cc7afc37f6738fe44f7c",
+    "url": "/static/css/main.08b983c9.chunk.css"
+  },
+  {
+    "revision": "2cdd77bc49e17269962a543026b5d86d",
+    "url": "/index.html"
+  }
+];
