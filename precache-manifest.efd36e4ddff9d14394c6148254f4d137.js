61689a89e65bcd00c9f2b5ddf716fedd42c2976f
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-06-16 21:52:05 +0200

    Updates

---
+++ precache-manifest.efd36e4ddff9d14394c6148254f4d137.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e01b25a457c75f6303c1",
+    "url": "/react-minesweeper/static/js/runtime~main.8612f2b5.js"
+  },
+  {
+    "revision": "e672c63075f628fc0a0f",
+    "url": "/react-minesweeper/static/js/main.f6cffaa5.chunk.js"
+  },
+  {
+    "revision": "8d6427c7ed60a6b3e392",
+    "url": "/react-minesweeper/static/js/2.fd8a59e3.chunk.js"
+  },
+  {
+    "revision": "e672c63075f628fc0a0f",
+    "url": "/react-minesweeper/static/css/main.08b983c9.chunk.css"
+  },
+  {
+    "revision": "1ea4bec8e9e94f0bf0e4970e5b0758ab",
+    "url": "/react-minesweeper/index.html"
+  }
+];
